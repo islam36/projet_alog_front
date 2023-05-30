b07e39dda30dfcfcc3e1babb15c772f08f4457f6
sofian <js_chelihi@esi.dz>
2023-05-30 16:10:03 +0100

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter,Route ,Routes } from 'react-router-dom'
+import Login from './Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login/>}/>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/patient/docs' element={<Login/>}></Route>
+        <Route path='/patient/viewDoctors' element={<Login/>}></Route>
+        <Route path='/patient/profile' element={<Login/>}></Route>
+        <Route path='/patient/profile' element={<Login/>}></Route>
+        <Route path='/patient/profile' element={<Login/>}></Route>
+        <Route path='/patient/profile' element={<Login/>}></Route>
+        <Route path='*'  element={<div>404 Not Found</div>}></Route>
+      </Routes>   
+    </BrowserRouter>
   )
 }
 
